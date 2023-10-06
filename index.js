32fb6b877fbd7d92f3686c7273d35c35f8a978af
Akash <akashathnure40@gmail.com>
2023-10-06 11:02:59 +0530

    rendering html files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const app = require('./app')
 const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
 
 // console.log('first')
 // console.log('x', app.x)
@@ -17,4 +19,23 @@ const dataControl = (req, res) => {
 }
 http.createServer(dataControl).listen(4500);
 
+
+// accessing html folder
+const htmlpath = path.join(__dirname, "html")
+
+// console.log('htmlpath', htmlpath)
+// app.use(express.static(htmlpath))
+
+app.get('', (req, resp) => {
+    resp.sendFile(`${htmlpath}/index.html`)
+})
+app.get('/about', (req, resp) => {
+    resp.sendFile(`${htmlpath}/about.html`)
+})
+
+app.get('*', (req, resp) => {
+    resp.sendFile(`${htmlpath}/pageNotFound.html`)
+})
+
+app.listen(5000)
 
